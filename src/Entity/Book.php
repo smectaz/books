85174fd52bdda44bb6bcf957131e78460d352646
smectaz <90684643+smectaz@users.noreply.github.com>
2022-09-14 00:37:42 +0200

    création du projet

---
+++ src/Entity/Book.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BookRepository::class)]
+class Book
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $coverText = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getCoverText(): ?string
+    {
+        return $this->coverText;
+    }
+
+    public function setCoverText(?string $coverText): self
+    {
+        $this->coverText = $coverText;
+
+        return $this;
+    }
+}

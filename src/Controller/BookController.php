627480762de6838e57f2a153c080cbd3e1388fc4
smectaz <90684643+smectaz@users.noreply.github.com>
2022-09-14 23:56:53 +0200

    route book id

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -11,12 +11,23 @@ use Symfony\Component\Serializer\SerializerInterface;
 
 class BookController extends AbstractController
 {
+    //route pour get tout les livres sans distinction
     #[Route('/api/books', name:'book', methods:['GET'])]
-function getBookList(BookRepository $bookRepository, SerializerInterface $serializer): JsonResponse
+    public function getBookList(BookRepository $bookRepository, SerializerInterface $serializer): JsonResponse
     {
     $bookList = $bookRepository->findAll();
     $jsonBookList = $serializer->serialize($bookList, 'json');
     return new JsonResponse($jsonBookList, Response::HTTP_OK, [], true);
 }
 
+//route pour get un livre par id
+    #[Route('/api/books/{id}', name: 'detailBook', methods: ['GET'])]
+    public function getDetailBook(int $id, SerializerInterface $serializer, BookRepository $bookRepository):JsonResponse{
+        $book = $bookRepository->find($id);
+        if ($book) {
+            $jsonBook = $serializer->serialize($book, 'json');
+            return new JsonResponse($jsonBook, Response::HTTP_OK, [], true);
+        }
+        return new JsonResponse(null, Response::HTTP_NOT_FOUND);
+    }
 }

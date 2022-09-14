a31760c8ee5ff9ca4932a99673cb6f5a1e6b2957
smectaz <90684643+smectaz@users.noreply.github.com>
2022-09-15 01:16:09 +0200

    correction pour avoir les routes d'auteur sans erreurs

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,21 +3,33 @@
 namespace App\DataFixtures;
 
 use App\Entity\Book;
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Author;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        //création des auteur
+        $listAuthor = [];
+        for ($i=1; $i <= 10 ; $i++) { 
+            $author = new Author();
+            $author -> setFirstName("Prénom " . $i);
+            $author -> setLastName("Nom " . $i);
+            $manager -> persist($author);
+            //sauvegarde des auteurs dans un tableau
+            $listAuthor[] = $author;
+        }
 
         //création d'une vingtaine de livre
 
         for ($i = 1; $i <= 20; $i++) {
-            $livre = new Book;
-            $livre->setTitle('livre ' . $i);
-            $livre->setCoverText('quatrième de couverture numéro : ' . $i);
-            $manager->persist($livre);
+            $book = new Book;
+            $book -> setTitle('livre ' . $i);
+            $book -> setCoverText('quatrième de couverture numéro : ' . $i);
+            $book -> setAuthor($listAuthor[array_rand($listAuthor)]);
+            $manager->persist($book);
         }
 
         $manager->flush();
--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -2,8 +2,9 @@
 
 namespace App\Entity;
 
-use App\Repository\BookRepository;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\BookRepository;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: BookRepository::class)]
 class Book
@@ -11,15 +12,19 @@ class Book
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(["getBooks"])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(["getBooks"])]
     private ?string $title = null;
 
     #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(["getBooks"])]
     private ?string $coverText = null;
 
     #[ORM\ManyToOne(inversedBy: 'books')]
+    #[Groups(["getBooks"])]
     private ?Author $author = null;
 
     public function getId(): ?int

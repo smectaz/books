31f408ae32ffd31950d2a43b6289d6d8e890e04b
smectaz <90684643+smectaz@users.noreply.github.com>
2022-09-16 19:56:40 +0200

    fixture user et install jwt

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,14 +3,37 @@
 namespace App\DataFixtures;
 
 use App\Entity\Book;
+use App\Entity\User;
 use App\Entity\Author;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private $userPasswordHasher;
+
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher) 
+    {
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
+        //création d'un user normal
+        $user = new User();
+        $user -> setEmail("user@bookapi.com");
+        $user -> setRoles(["ROLE_USER"]);
+        $user -> setPassword($this -> userPasswordHasher -> hashPassword($user, "password"));
+        $manager -> persist($user);
+
+        //création d'un user admin
+        $userAdmin = new User();
+        $userAdmin -> setEmail("admin@bookapi.com");
+        $userAdmin -> setRoles(["ROLE_ADMIN"]);
+        $userAdmin -> setPassword($this -> userPasswordHasher -> hashPassword($userAdmin, "password"));
+        $manager -> persist($userAdmin);
+
         //création des auteur
         $listAuthor = [];
         for ($i=1; $i <= 10 ; $i++) { 
